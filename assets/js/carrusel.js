f84860826310a301b913a257b69f3bd262d2b281
Tadeo <tade25.molinas@gmail.com>
2025-03-06 10:52:48 -0300

    first commit

---
+++ assets/js/carrusel.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('.banners-carrusel').slick({
+        dots: true,
+        infinite: true,
+        arrows: false,
+        autoplay:true,
+        autoplaySpeed: 3000,
+        speed: 1000,
+        pauseOnHover: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    });
+
+});
